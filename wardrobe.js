e8e7fb8e3c3690af7766ca42efe8878346cefcf8
nulcow <88356560+nulcow@users.noreply.github.com>
2024-05-31 17:52:23 -0400

    Put the three.js canvas inside a div

--- wardrobe.js
+++ wardrobe.js
@@ -4,15 +4,15 @@ import * as THREE from 'three';
 
 /* === PREVIEW HANDLER === */
 
-const previewCanvas = document.getElementById("previewImage")
+const previewContainer = document.getElementById("previewImageContainer")
 
 // Three.js scene setup
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, previewCanvas.width / previewCanvas.height, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, previewContainer.width / previewContainer.height, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( previewCanvas.width, previewCanvas.height );
-document.body.appendChild( renderer.domElement );
+renderer.setSize( previewContainer.width, previewContainer.height );
+previewContainer.appendChild( renderer.domElement );
 
 // Create geometry
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
